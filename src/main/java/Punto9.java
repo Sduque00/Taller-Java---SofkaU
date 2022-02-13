b5cea8739f3785bce67b4ff66dd4d8aa96c676d7
sduque <samuduke00@gmail.com>
2022-02-13 18:03:21 -0500

    punto 9

---
+++ src/main/java/Punto9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Punto9 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String frase = "La sonrisa sera la mejor arma contra la tristeza";
+        String nuevaFrase;
+
+        System.out.println(frase.replace("a","e"));
+
+        System.out.println("Ingrese la nueva frase");
+        nuevaFrase = scanner.nextLine();
+
+        System.out.println("\n\"" + frase + ", " + nuevaFrase + "\"");
+
+    }
+}
